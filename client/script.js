e727e286a351220b77f2e39a453017c9b13c7453
albertgoncalves <goncalves.albert@gmail.com>
2019-09-21 18:26:30 -0400

    client echo skeleton

--- client/script.js
+++ client/script.js
@@ -1,5 +1,35 @@
-function main() {
-    console.log("Hello, world!");
+var wsUri = "wss://echo.websocket.org/";
+var output;
+
+function inscribe(text) {
+    var pre = document.createElement("p");
+    pre.style.wordWrap = "break-word";
+    pre.innerHTML = text;
+    output.appendChild(pre);
 }
 
-main();
+function testWebSocket() {
+    websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) {
+        inscribe("CONNECTED");
+        var message = "Hello, world!";
+        inscribe("SENT: " + message);
+        websocket.send(message);
+    };
+    websocket.onclose = function(evt) {
+        inscribe("DISCONNECTED");
+    };
+    websocket.onmessage = function(evt) {
+        inscribe("<span style=\"color: blue;\">RESPONSE: " + evt.data +
+                 "</span>");
+        websocket.close();
+    };
+    websocket.onerror = function(evt) {
+        inscribe("<span style=\"color: red;\">ERROR:</span> " + evt.data);
+    };
+}
+
+window.addEventListener("load", function() {
+    output = document.getElementById("output");
+    testWebSocket();
+}, false);
