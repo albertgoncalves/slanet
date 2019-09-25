fb94fda7fe72b328d4e22f86591cd3df313c9370
albertgoncalves <goncalves.albert@gmail.com>
2019-09-24 23:51:06 -0400

    refine tests

--- server/src/combo/combo.go
+++ server/src/combo/combo.go
@@ -7,20 +7,22 @@ const (
 
 func combinations(n int) [][K]int {
     result := make([][K]int, 0)
-    var state [K]int
-    var closure func(int, int)
-    closure = func(j, k int) {
-        for i := k; i < n; i++ {
-            state[j] = i
-            if j == L {
-                var combination [K]int = state
-                result = append(result, combination)
-            } else {
-                closure(j+1, i+1)
+    if 0 < n {
+        var state [K]int
+        var closure func(int, int)
+        closure = func(j, k int) {
+            for i := k; i < n; i++ {
+                state[j] = i
+                if j == L {
+                    var combination [K]int = state
+                    result = append(result, combination)
+                } else {
+                    closure(j+1, i+1)
+                }
             }
+            return
         }
-        return
+        closure(0, 0)
     }
-    closure(0, 0)
     return result
 }
--- server/src/combo/combo_test.go
+++ server/src/combo/combo_test.go
@@ -5,30 +5,30 @@ import (
 )
 
 func TestCombinations(t *testing.T) {
-    func() {
-        result := combinations(1)
-        if 0 < len(result) {
-            t.Error("combinations(2)")
+    for i := -1; i < 3; i++ {
+        result := combinations(i)
+        if !(0 == len(result)) {
+            t.Errorf("len(combinations(%d))", i)
         }
-    }()
-    func() {
-        result := combinations(2)
-        if 0 < len(result) {
-            t.Error("combinations(2)")
-        }
-    }()
+    }
     func() {
         result := combinations(3)
+        if !(1 == len(result)) {
+            t.Error("len(combinations(3))")
+        }
         expected := [K]int{0, 1, 2}
         for i := 0; i < K; i++ {
             if !(result[0][i] == expected[i]) {
-                t.Error("combinations(3)")
+                t.Errorf("combinations(3)[0][%d]", i)
                 return
             }
         }
     }()
     func() {
         result := combinations(4)
+        if !(4 == len(result)) {
+            t.Error("len(combinations(4))")
+        }
         expected := [4][K]int{
             {0, 1, 2},
             {0, 1, 3},
@@ -38,7 +38,7 @@ func TestCombinations(t *testing.T) {
         for i := 0; i < 4; i++ {
             for j := 0; j < K; j++ {
                 if !(result[i][j] == expected[i][j]) {
-                    t.Error("combinations(4)")
+                    t.Errorf("combinations(4)[%d][%d]", i, j)
                     return
                 }
             }
@@ -46,6 +46,9 @@ func TestCombinations(t *testing.T) {
     }()
     func() {
         result := combinations(5)
+        if !(10 == len(result)) {
+            t.Error("len(combinations(5))")
+        }
         expected := [10][K]int{
             {0, 1, 2},
             {0, 1, 3},
@@ -61,10 +64,16 @@ func TestCombinations(t *testing.T) {
         for i := 0; i < 10; i++ {
             for j := 0; j < K; j++ {
                 if !(result[i][j] == expected[i][j]) {
-                    t.Error("combinations(5)")
+                    t.Errorf("combinations(5)[%d][%d]", i, j)
                     return
                 }
             }
         }
     }()
+}
+
+func BenchmarkCombinations(b *testing.B) {
+    for i := 0; i < b.N; i++ {
+        combinations(9)
+    }
 }
