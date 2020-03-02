f1e286ac4f29bae861e9d3ed76f21dc6d2187553
Albert Goncalves <goncalves.albert@gmail.com>
2020-03-01 23:11:36 -0500

    slightly more portable

--- server/main.go
+++ server/main.go
@@ -3,12 +3,17 @@ package main
 import (
     "log"
     "os"
+    "path"
 
     "server"
 )
 
 func main() {
-    err := server.Run("../client/src", os.Getenv("HOST"), os.Getenv("PORT"))
+    err := server.Run(
+        path.Join("..", "client", "src"),
+        os.Getenv("HOST"),
+        os.Getenv("PORT"),
+    )
     if err != nil {
         log.Fatal(err)
     }
